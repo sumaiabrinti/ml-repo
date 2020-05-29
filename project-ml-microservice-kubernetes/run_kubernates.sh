9f80c617c69232dff8a9bc11aff97bd2daab06db
root <root@ip-172-31-21-93.ap-southeast-1.compute.internal>
2020-05-29 18:09:33 +0000

    upload_docker.sh

---
+++ project-ml-microservice-kubernetes/run_kubernates.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=sumaiabrint/ml-repo
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl create deployment ml-repo-cluster --image=$dockerpath
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl proxy
+export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+echo Name of the Pod: $POD_NAME
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
 
-# This tags and uploads an image to Docker Hub
+# Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
-# This is your Docker ID/path
- dockerpath=sumaiabrinti/ml-repo                                                                                        
-# Step 2
-# Run the Docker Hub container with kubernetes
-kubectl create deployment ml-repo-cluster --image=$dockerpath
+dockerpath=sumaiabrinti/ml-repo
 
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login --username sumaiabrinti && docker tag ml-repo:latest $dockerpath
 # Step 3:
-# List kubernetes pods
-kubectl get pods                                                                                                        
-# Step 4:
-# Forward the container port to a host
-kubectl proxy
-export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
-echo Name of the Pod: $POD_NAME
-
+# Push image to a docker repository
+docker push $dockerpath
